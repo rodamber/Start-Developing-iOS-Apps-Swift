62783a2668afce3e22d186531532e8f4aa808419
rodamber <rodrigoambernardo@gmail.com>
2015-12-15 19:29:47 +0000

    Finished the "Define Your Data Model" section.

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,27 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Rodrigo Bernardo on 15/12/2015.
+//
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name   : String
+    var photo  : UIImage?
+    var rating : Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name   = name
+        self.photo  = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
